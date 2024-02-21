8dfabf3ccd90710f802c1f90b2572de4ed185ce4
BlagoTcherecharov <117592898+BlagoTcherecharov@users.noreply.github.com>
2024-02-21 10:53:05 +0200

    Create Print array with pointer.cpp

---
+++ Pointers/Print array with pointer.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+void main()
+{
+	const int SIZE = 10;
+
+	int array[SIZE];
+
+	for (size_t i = 0; i < SIZE; i++)
+	{
+		cin >> array[i];
+	}
+
+	for (size_t i = 0; i < SIZE; i++)
+	{
+		cout << array[i] << " ";
+	}
+
+	cout << endl;
+
+	for (size_t i = 0; i < SIZE; i++)
+	{
+		cout << *(array + i) << " ";
+	}
+}
